ef38645e8c8417f0df5bf594a893e9108480902b
FX-XXX <1290975745@qq.com>
2017-03-03 10:25:22 +0800

    stitpweb

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>登录中</title>
+</head>
+
+<body>
+<?php
+// session_start();
+$username=$_POST['Username'];//接收数据
+$passwd=$_POST['Password'];
+if((isset($username))&&(isset($passwd))){  //是否输入
+   echo "<script language=\"JavaScript\">alert(\"hello\");</script>";  
+   $con = mysql_connect("127.0.0.1:3306","root");
+    if (!$con)
+    {
+        die('Could not connect: ' . mysql_error());
+    }
+mysql_select_db("stitp",$con);
+$dbusername=null;
+$dbpassword=null;
+$result=mysql_query("select * from users where user_name='".$username."';");
+$res = mysql_fetch_array($result);//查询结果
+$dbusername=$res[1];
+$dbpassword=$res[2];
+if($dbusername==null){//如果为空 返回主页面
+    echo "<script language=\"JavaScript\">alert(\"no user!\");</script>";
+    header("Location:index.html");
+ }
+elseif($dbpassword!==$passwd){//如果密码错
+    header("Location:index.html");
+ }else{
+    header("Location:menu.html"); //正确跳转
+ }
+ }
+mysql_close($con);//关闭数据库
+?>
+
+</body>
+</html>
---
+++ ser.php
@@ -0,0 +1,51 @@
+<?php
+$myfile = fopen("initial1.txt", "r") or die("Unable to open file!");
+$newfile = fopen("homechar.txt", "w") or die("Unable to open file!");
+//只读打开格式文件，清空写打开最终表格文件
+	$con = mysql_connect("127.0.0.1:3306","root","");
+	if (!$con)
+    {
+        die('Could not connect: ' . mysql_error());
+    }
+    // $nowdata = date("Ymd");//当天的日期
+	mysql_select_db("stitp",$con);
+	$querry = "select * from alle order by date DESC";//降序查询
+    $sql = mysql_query($querry,$con);
+    $i = 0;
+    $finelec = "";//为避免报warning先生成空字符
+    $findate = "";
+    //select * from alle order by date DESC;降序
+    while(($result = mysql_fetch_array($sql))&&($i<15)){//取最近15天的数据
+        $res[$i]=$result;
+        $data = $res[$i][0];//返回的日期
+        $elec = $res[$i][1];//返回的电量
+        if($i<14){
+            //倒序的拼接字符串，所以最后一天前面不加逗号
+            $finelec = ",".$elec.$finelec;
+            $findate = ",\"".$data."\"".$findate;//分号前加转义字符
+        }else{
+            $finelec = $elec.$finelec;
+            $findate = "\"".$data."\"".$findate;
+        }
+        $i++;
+    }
+    $line1 = 1;//行数计数
+    while (!feof($myfile)) {//feof判断是否读完文件
+        if ($line1 == 15) {
+            fgets($myfile);//虽然源文件这一行不读但是指针向下走 
+            fwrite($newfile,$finelec);//字符串读入特定行
+        }elseif($line1 == 23){
+            fgets($myfile);
+            fwrite($newfile,$findate);
+        }else{
+            $newline = fgets($myfile);
+            fwrite($newfile,$newline);//源文件拷贝进新文件
+        }
+        //15行和23行存放特定值
+        $line1++;
+    }
+    mysql_close($con);
+fclose($myfile);//关闭文件
+fclose($newfile);
+echo "hello";
+?>
